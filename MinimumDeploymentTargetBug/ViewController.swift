aeb647dfe3efaae89b6f701469c602865debfa14
Paul Young <paulyoungonline@gmail.com>
2015-09-23 01:00:36 -0700

    Add example project.

---
+++ MinimumDeploymentTargetBug/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MinimumDeploymentTargetBug
+//
+//  Created by Paul Young on 9/23/15.
+//  Copyright © 2015 Paul Young. All rights reserved.
+//
+
+import UIKit
+import ImgFlo
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Minimum Deployment Target Bug
+
+Run `script/build` to see intermittent build failures of the nature `error: module file's minimum deployment target is ios9.0 v9.0` for `ImgFlo` despite its minimum deployment target being set to 8.0
